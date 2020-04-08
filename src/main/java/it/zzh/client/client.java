15eaef4a008bab65fdd26f01b3e1c6a5eff6bec0
ChenxiZZZ <53244724+ChenxiZZZ@users.noreply.github.com>
2020-04-08 16:05:40 +0800

    Add files via upload

---
+++ src/main/java/it/zzh/client/client.java
@@ -0,0 +1,20 @@
+package it.zzh.client;
+
+import it.zzh.domain.person;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.annotation.Resource;
+
+public class client {
+    @Resource(name="person")
+    private static person p2=null;
+    public static void main(String[] args) {
+        ApplicationContext context=new ClassPathXmlApplicationContext("bean.xml");
+        person p1=context.getBean("person",person.class);
+        System.out.println(p1);
+        if(p2!=null)
+            System.out.println(p2);
+
+    }
+}
---
+++ src/main/java/it/zzh/domain/person.java
@@ -0,0 +1,22 @@
+package it.zzh.domain;
+
+public class person {
+    private int age;
+    private String name;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
